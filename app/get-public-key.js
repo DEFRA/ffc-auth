9c6dbb92cc8dcb091f600e4d4fffa802a2d78a0c
John Watson <john.watson1@defra.gov.uk>
2023-12-07 14:42:30 +0000

    Update config

--- app/get-public-key.js
+++ app/get-public-key.js
@@ -1,5 +1,5 @@
 const Wreck = require('@hapi/wreck')
-const { authHost } = require('../config')
+const { authHost } = require('./config')
 
 const getPublicKey = async () => {
   const { payload } = await Wreck.get(`${authHost}/auth/public-key`, { json: true })
--- app/validate-token.js
+++ app/validate-token.js
@@ -1,5 +1,5 @@
 const Wreck = require('@hapi/wreck')
-const { authHost } = require('../config')
+const { authHost } = require('./config')
 
 const validateToken = async (decoded, _request, _h) => {
   const { payload } = await Wreck.post(`${authHost}/auth/validate`, {
