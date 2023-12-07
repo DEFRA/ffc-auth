1779134098788b3e43efaa5a79bcb2bd5b3aaae9
John Watson <john.watson1@defra.gov.uk>
2023-12-07 14:49:29 +0000

    Export scopes

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const { mapAuth } = require('./map-auth')
 const { getPublicKey } = require('./get-public-key')
 const { validateToken } = require('./validate-token')
+const { scopes } = require('./scopes')
 
 module.exports = {
   mapAuth,
   getPublicKey,
-  validateToken
+  validateToken,
+  scopes
 }
