c0a9ef4099a44666e64655cdb2f8a741687762a4
Rana Salem <ranatasalem@gmail.com>
2024-02-09 16:07:05 +0000

    update refresh-access-token.js

--- app/refresh-access-token.js
+++ app/refresh-access-token.js
@@ -1,26 +1,14 @@
 const Wreck = require('@hapi/wreck')
+const { authHost } = require('./config')
 
-const refreshAccessToken = async (refreshToken, config) => {
-  const { payload: wellKnown } = await Wreck.get(config.wellKnownUrl, {
-    json: true
-  })
-
-  const query = [
-    `client_id=${config.clientId}`,
-    `client_secret=${config.clientSecret}`,
-    'grant_type=refresh_token',
-    `scope=openid offline_access ${config.clientId}`,
-    `refresh_token=${refreshToken}`,
-    `redirect_uri=${config.redirectUrl}`
-  ].join('&')
-
-  const { payload } = await Wreck.post(`${wellKnown.url}?${query}`, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+const refreshAccessToken = async (decoded, _request, _h) => {
+  const { payload } = await Wreck.get(`${authHost}/auth/refresh`, {
+    payload: {
+      token: decoded,
+      refreshToken: decoded
     },
     json: true
   })
-
   return payload
 }
 
