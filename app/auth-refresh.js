a167ad350521ee5628e84f73d578bdfa566aa79e
Rana Salem <ranatasalem@gmail.com>
2024-02-09 10:15:21 +0000

    create auth-refresh.js

---
+++ app/auth-refresh.js
@@ -0,0 +1,26 @@
+const { options } = require('joi')
+
+const authRefresh = () => {
+  register: (server, options) => {
+    server.ext('onPostAuth', async (request, h) => {
+      if (!authConfig.defraIdEnabled) {
+        return h.continue
+      }
+
+      if (request.path.startsWith('/assets/')) {
+        return h.continue
+      }
+
+      const currentToken = request.state[AUTH_COOKIE_NAME]
+      const refreshToken = request.state[AUTH_REFRESH_COOKIE_NAME]
+
+      if (!currentToken || !refreshToken) {
+        return h.continue
+      }
+
+      const response = await refreshAccessToken(request.state[AUTH_REFRESH_COOKIE_NAME])
+      h.state(AUTH_COOKIE_NAME, response.access_token, authConfig.cookieOptions)
+      return h.continue
+    })
+  }
+}
