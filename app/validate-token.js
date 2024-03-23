13a717124fc71f61b2e6195b72592bc885991a7b
John Watson <john.watson1@defra.gov.uk>
2024-03-23 17:50:32 +0000

    Update validate-token.js and package.json

--- app/validate-token.js
+++ app/validate-token.js
@@ -1,9 +1,11 @@
 const Wreck = require('@hapi/wreck')
 const { authHost } = require('./config')
+const { SESSION_COOKIE_NAME } = require('./constants/cookies')
 
-const validateToken = async (decoded, _request, _h) => {
+const validateToken = async (decoded, request, _h) => {
   const { payload } = await Wreck.post(`${authHost}/auth/validate`, {
     payload: {
+      sessionId: request.state[SESSION_COOKIE_NAME],
       token: decoded
     },
     json: true
