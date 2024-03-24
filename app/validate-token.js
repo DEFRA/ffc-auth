bba5f1e5137979837e4da6c0e6b3dc32eb0c2732
John Watson <john.watson1@defra.gov.uk>
2024-03-24 11:52:47 +0000

    Update session key

--- app/validate-token.js
+++ app/validate-token.js
@@ -5,7 +5,7 @@ const { SESSION_COOKIE_NAME } = require('./constants/cookies')
 const validateToken = async (decoded, request, _h) => {
   const { payload } = await Wreck.post(`${authHost}/auth/validate`, {
     payload: {
-      sessionId: request.state[SESSION_COOKIE_NAME],
+      session: request.state[SESSION_COOKIE_NAME],
       token: decoded
     },
     json: true
