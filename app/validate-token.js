e041f3e40e5e1aec1ff79a5b347228f736ffe27a
John Watson <john.watson1@defra.gov.uk>
2024-03-24 16:50:54 +0000

    Add refresh redirect

--- app/validate-token.js
+++ app/validate-token.js
@@ -2,7 +2,11 @@ const Wreck = require('@hapi/wreck')
 const { authHost } = require('./config')
 const { SESSION_COOKIE_NAME } = require('./constants/cookies')
 
-const validateToken = async (decoded, request, _h) => {
+const validateToken = async (decoded, request, h) => {
+  if (decoded.exp * 1000 - Date.now() <= 600 * 1000) {
+    return h.redirect(`${authHost}/auth/refresh?redirect=${request.url.pathname}`)
+  }
+
   const { payload } = await Wreck.post(`${authHost}/auth/validate`, {
     payload: {
       session: request.state[SESSION_COOKIE_NAME],
